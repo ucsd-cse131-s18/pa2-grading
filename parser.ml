502d75f4dbd2163f0c3f1f965c83eb72126bfc38
Andrew Yang <yuy130@ucsd.edu>
2018-04-18 19:56:00 -0700

    Update parser.ml

--- parser.ml
+++ parser.ml
@@ -5,7 +5,7 @@ open Expr
 let boa_max = int_of_float(2.**30.) - 1;;
 let boa_min = -int_of_float(2.**30.);;
 let valid_id_regex = Str.regexp "[a-zA-Z][a-zA-Z0-9]*"
-let number_regex = Str.regexp "^[-]?[0-9]+"
+let number_regex = Str.regexp "^[+-]?[0-9]+"
 let reserved_words = ["let"; "add1"; "sub1"; "isNum"; "isBool"; "if"]
 let reserved_constants = ["true"; "false"; ]
 let int_of_string_opt s =
